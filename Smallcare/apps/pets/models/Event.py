f823b468404db06194fad872ca2ac94a5ecbaf5e
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2020-11-12 22:49:08 +0200

    12.11.2002 Last

--- Smallcare/apps/pets/models/Event.py
+++ Smallcare/apps/pets/models/Event.py
@@ -1,5 +1,13 @@
 from django.db import models
+from .Animal import Animal
+
+EventEnum = (('r', 'request'), ('p', 'pending'), ('d', 'done'))
 
 
 class Event(models.Model):
-    pass
+    animal_id = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    price = models.IntegerField()
+    description = models.TextField()
+    status = models.CharField(max_length=1, choices=EventEnum)
--- Smallcare/apps/pets/urls.py
+++ Smallcare/apps/pets/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('test/', views.test, name='index'),
-    path('animals/', views.my, name='my'),
+    path('animal/', views.my, name='my'),
 ]
