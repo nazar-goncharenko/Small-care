37db0abea1ac337a66610f09c8c5bc209f317619
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2020-11-12 20:36:02 +0200

    Uptader links

--- Smallcare/apps/pets/views.py
+++ Smallcare/apps/pets/views.py
@@ -1,8 +1,8 @@
-
 from django.shortcuts import render
 from django.http import response, HttpResponse
 from django.shortcuts import render
 from .models import Animal
+
 
 def index(request):
     return render(request, 'pets/index.html')
@@ -11,6 +11,7 @@ def index(request):
 def test(request):
     return render(request, 'pets/test.html')
 
+
 def my(request):
     list_of_pets = Animal.Animal.objects.all()
-    return render(request, 'base.html', {'list_of_pets': list_of_pets})
+    return render(request, 'list.html', {'list_of_pets': list_of_pets})
