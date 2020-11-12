3a33dabe2918405ec50245d5694b1a83f2f08366
oleslav <oleslav.boychuk@gmail.com>
2020-11-12 22:10:57 +0200

    Move ForeignKey from user to animal

--- Smallcare/apps/pets/models/User.py
+++ Smallcare/apps/pets/models/User.py
@@ -1,5 +1,4 @@
 from django.db import models
-from . import Animal
 # from . import Event
 
 
@@ -9,7 +8,6 @@ class User(models.Model):
 
     description = models.TextField()
     photo = models.CharField(max_length=50)
-    animal = models.ForeignKey(Animal.Animal, on_delete=models.CASCADE)
 
     mail = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=15)
