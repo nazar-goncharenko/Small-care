8da05027d660e426be6cfc058f3f03438ac991c4
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2020-11-13 14:42:46 +0200

    Delete Django-project

--- Smallcare/apps/pets/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Animal, User, Event
-
-admin.site.register(Animal.Animal)
-admin.site.register(User.User)
-admin.site.register(Event.Event)
-# Register your models here.
