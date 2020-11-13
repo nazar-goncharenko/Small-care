fc39e358109761c43ee15d5f7021639fe1dd4035
ViraMaximets <viramaximets@gmail.com>
2020-11-13 10:00:24 +0200

    Added field names

--- Smallcare/apps/pets/models/Event.py
+++ Smallcare/apps/pets/models/Event.py
@@ -3,11 +3,10 @@ from .Animal import Animal
 
 EventEnum = (('r', 'request'), ('p', 'pending'), ('d', 'done'))
 
-
 class Event(models.Model):
     animal_id = models.ForeignKey(Animal, on_delete=models.CASCADE)
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    price = models.IntegerField()
-    description = models.TextField()
+    start_time = models.TimeField('Час початку')
+    end_time = models.TimeField('Час закінчення')
+    price = models.IntegerField('Ціна')
+    description = models.TextField('Опис')
     status = models.CharField(max_length=1, choices=EventEnum)
