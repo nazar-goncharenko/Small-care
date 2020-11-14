ec482da1f8792e1eae3c2ade7cc2205aefb7f7c0
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2020-11-14 23:21:21 +0200

    Creating user

--- src/main/java/Smallcare/Controllers/MainController.java
+++ src/main/java/Smallcare/Controllers/MainController.java
@@ -3,12 +3,14 @@ package Smallcare.Controllers;
 
 import Smallcare.Models.User;
 import Smallcare.Repositories.UserRepository;
+import Smallcare.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,14 +18,33 @@ import java.util.List;
 public class MainController {
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
+
+    @GetMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+
+    @PostMapping("/user")
+    public String createUser(Model model,
+                             @RequestParam(value = "firstName") String firstName,
+                             @RequestParam(value = "lastName") String lastName,
+                             @RequestParam(value = "email") String email,
+                             @RequestParam(value = "password") String password,
+                             @RequestParam(value = "phoneNumber") String phoneNumber,
+                             @RequestParam(value = "city", required = false) String city,
+                             @RequestParam(value = "district", required = false) String district,
+                             @RequestParam(value = "description", required = false) String description){
+        userService.create(firstName, lastName, email, password, phoneNumber, city, district, description);
+        return "redirect:/";
+    }
 
 
     @RequestMapping("/users")
     public String all(Model model){
         System.out.println("users");
-        List<User> users = userRepository.findAll();
+        List<User> users = userService.getAll();
         if (!users.isEmpty()) {
             model.addAttribute("users", users);
         }
