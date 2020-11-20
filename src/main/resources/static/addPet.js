8758429f2269afc7577153a01f49e95cce8351b6
oleslav <oleslav.boychuk@gmail.com>
2020-11-21 00:03:02 +0200

    Check if sending file is image
    I added form validation to check if sending file is image or not, if it isn't the file cant be sended

--- src/main/resources/static/addPet.js
+++
@@ -1,6 +0,0 @@
-function isFileImage(input) {
-    console.log(input);
-    let file = input.files[0];
-    console.log(file && file['type'].split('/')[0] === 'image');
-    return file && file['type'].split('/')[0] === 'image';
-}
