47a439613f0a79e31b0385e2ed1e7dde0a42561d
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2020-11-21 21:23:46 +0200

    Added Security

--- src/main/java/Smallcare/Configs/MvcConfig.java
+++ src/main/java/Smallcare/Configs/MvcConfig.java
@@ -3,6 +3,7 @@ package Smallcare.Configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,9 +11,16 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Value("${upload.path}")
     String upload;
+
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:/" + upload + "/");
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
     }
 }
--- src/main/java/Smallcare/Controllers/UserController.java
+++ src/main/java/Smallcare/Controllers/UserController.java
@@ -6,6 +6,7 @@ import Smallcare.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
