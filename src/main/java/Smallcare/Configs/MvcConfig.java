69889564ab5865943eec1933c462d27fe0ac0d95
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2020-12-03 18:32:17 +0200

    Update MvcConfig.java

--- src/main/java/Smallcare/Configs/MvcConfig.java
+++ src/main/java/Smallcare/Configs/MvcConfig.java
@@ -12,7 +12,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Value("${upload.path}")
     String upload;
 
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
