a272d85e89f797d376b026d6bbff11b4196bede6
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2020-12-10 22:57:10 +0200

    Added Internationalization

---
+++ src/main/java/Smallcare/Controllers/PageController.java
@@ -0,0 +1,13 @@
+package Smallcare.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/international")
+    public String getInternationalPage() {
+        return "international";
+    }
+}
