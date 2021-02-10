4849723e03367ebb0917499eb725eaeb795531f6
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2021-02-10 13:02:51 +0200

    Translation to ukr

--- src/main/resources/static/js/createEvent.js
+++ src/main/resources/static/js/createEvent.js
@@ -2,7 +2,7 @@ function myValidationForm() {
     if($('html :checkbox:checked').length > 0){
         return true;
     } else {
-        alert('Ви не можете створити без тваринки');
+        alert('Ви не можете створити подію без тваринки');
         return false;
     }
 }
